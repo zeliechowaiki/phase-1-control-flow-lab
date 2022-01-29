0654a0346876f998448e74958b1fd17994b4d88d
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-01-28 23:07:04 -0500

    commit tree d211d8689320cc93138fbd7dbfccb8b66cbd7023

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  }
 }
